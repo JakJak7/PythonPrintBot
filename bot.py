f6dfdc293639be182524c67805a9d3cc28ef1d1e
JakJak <jakob.bjerre@gmail.com>
2019-07-01 21:41:52 +0200

    Send message with inline buttons and read result

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import Bot, InlineKeyboardMarkup, InlineKeyboardButton
 from PIL import Image
 from subprocess import call
 import requests
 import logging
 import configparser
+import json
 
 from wifi_bootstrap import listen_for_connection
 
@@ -22,24 +23,29 @@ def photo(bot, update):
     photo_file = bot.get_file(file_id)
     file_location = 'files/' + file_id + '.jpg'
     photo_file.download(file_location)
-    update.message.reply_text('Printing image!')
-    r = lighten_image(file_id, '.jpg', text)
+    #update.message.reply_text('Printing image!')
+    handle_image(bot, update, file_id, '.jpg', text)
 
 def sticker(bot, update):
     file_id = update.message.sticker.file_id
     sticker_file = bot.get_file(file_id)
     file_location = 'files/' + file_id + '.webp'
     sticker_file.download(file_location)
-    update.message.reply_text('Printing sticker!')
+    #update.message.reply_text('Printing sticker!')
     convert_webp(file_id, file_location)
-    r = lighten_image(file_id, '.png', '20')
+    handle_image(bot, update, file_id, '.png', '20')
+
+def handle_image(bot, update, file_id, file_extension, percentage):
+    file_name = lighten_image(file_id, file_extension, percentage)
+    ask_master(bot, "username:DDD", update.message.chat_id, file_name)
+    #return print_label(file_name)
 
 def convert_webp(file_id, file_location):
     call(['dwebp', file_location, '-o', 'files/' + file_id + '.png'])
 
-def print_label(file_id, file_extension):
+def print_label(file_name):
     url = config['BOT_SECRETS']['BaseUrl'] + 'api/print/image'
-    file = {'upload': open('files/' + file_id + file_extension, 'rb')}
+    file = {'upload': open('files/' + file_name, 'rb')}
     r = requests.post(url, files=file)
     return r.status_code == 200
 
@@ -48,7 +54,7 @@ def lighten_image(file_id, file_extension, percentage):
     new_file_id = file_id + '_light' + percentage
     new_file_path = 'files/' + new_file_id + file_extension
     call(['convert', file_path, '-fill', 'white', '-colorize', percentage+'%', new_file_path])
-    return print_label(new_file_id, file_extension)
+    return new_file_id + file_extension
 
 def start(bot, update):
     send_text(bot, update, 'Send me stickers!')
@@ -58,6 +64,20 @@ def message(bot, update):
 
 def send_text(bot, update, message):
     bot.send_message(chat_id=update.message.chat_id, text=message)
+
+def ask_master(bot, user_name, chat_id, file_name):
+    dataA = str(chat_id)+";"+file_name+";a"
+    dataB = str(chat_id)+";"+file_name+";b"
+    items = [[InlineKeyboardButton(text="Allow", callback_data=dataA), InlineKeyboardButton(text="Deny", callback_data=dataB)]]
+    reply_markup = InlineKeyboardMarkup(inline_keyboard=items)
+    message = user_name + "(" + str(chat_id) + ")"
+    bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
+
+def send_to_master(text):
+    get_bot().send_message(chat_id=config['BOT_SECRETS']['MasterUserId'], text=text)
+
+def get_bot():
+    return Bot(token=config['BOT_SECRETS']['Token'])
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -77,11 +97,18 @@ dp.add_handler(sticker_handler)
 text_handler = MessageHandler(Filters.text, message)
 dp.add_handler(text_handler)
 
+def callback_handler(bot, query):
+    print(query.callback_query.data)
+    things = query.callback_query.data.split(';')
+    if (things[2] == "a"):
+        print_label(things[1])
+
+dp.add_handler(CallbackQueryHandler(callback_handler))
+
 def main():
     my_ip = listen_for_connection().decode('utf-8')
     print(my_ip)
-    bot = Bot(token=config['BOT_SECRETS']['Token'])
-    bot.send_message(chat_id=config['BOT_SECRETS']['MasterUserId'], text=my_ip)
+    send_to_master(my_ip)
     updater.start_polling()
     updater.idle()
 
